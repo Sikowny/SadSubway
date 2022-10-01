677db495fa9a60f3bfccdd57c3beec3dd3d3d2b6
Sikowny <bsickowny@hotmail,com>
2022-09-30 17:20:21 -0700

    movement changes

--- Main.gd
+++ Main.gd
@@ -4,11 +4,12 @@ extends Node
 # Declare member variables here. Examples:
 # var a = 2
 # var b = "text"
+
 var accelX = 1;
-var MAX_VEL_X = 5;
-var MAX_VEL_Y = 5;
-var velX = 0;
-var velY = 0;
+var MAX_VEL_X = 3;
+var MAX_VEL_Y = 3;
+var vel2D = Vector2();
+var Move = Vector2();
 
 
 
@@ -21,20 +22,18 @@ func _ready():
 func _process(delta):
 	var MoveX = Input.get_action_strength("ui_right") - Input.get_action_strength("ui_left")
 	var MoveY = Input.get_action_strength("ui_down") - Input.get_action_strength("ui_up")
+
+	Move = Vector2(MoveX,MoveY).normalized()
 	
-	var vector = Vector2(MoveX,MoveY).normalized()
-	velX += vector.x*accelX;
-	velY += vector.y;
+	vel2D.x += Move.x*accelX;
+	vel2D.y += Move.y;
 	
 	#friction
-	if(MoveX == 0): velX = 0;
-	if(MoveY == 0): velY = 0;
+	if(Move.x == 0): vel2D.x = 0;
+	if(Move.y == 0): vel2D.y = 0;
 	
-	#velX = clamp(velX, -MAX_VEL_X, MAX_VEL_X)
+	vel2D.x = clamp(vel2D.x , -MAX_VEL_X, MAX_VEL_X)
+	vel2D.y = clamp(vel2D.y, -MAX_VEL_Y, MAX_VEL_Y)
 	
-	if(abs(velX) > MAX_VEL_X): velX = MAX_VEL_X * sign(velX)
-	if(abs(velY) > MAX_VEL_Y): velY = MAX_VEL_Y * sign(velY)
 	
-	var Move = Vector2(velX, velY)
-	
-	$Sprite.position += Move
+	$Sprite.position += vel2D
