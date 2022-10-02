e8ed12f9d508f0380d2e622f1cb01d1fef2f7e7e
Sikowny <bsickowny@hotmail,com>
2022-10-01 18:20:28 -0700

    Platformer updates. Coin, Tileset

---
+++ Scenes/Objects/Coin.gd
@@ -0,0 +1,29 @@
+extends Area2D
+
+# Based on the coin code from 2D Platformer Toolkit!
+
+#onready var audio = $AudioStreamPlayer
+
+var collected = false
+
+func _ready():
+	visible = true
+	#Global.init_coin()
+
+func collect():
+	visible = false
+	collected = true
+	
+	#if Global.max_coins != 1:
+		#audio.pitch_scale = (float(Global.current_coins) / (Global.max_coins - 1)) + 1
+	#print(audio.pitch_scale)
+	#Global.collect_coin()
+	#audio.play(0)
+	
+
+func _on_Coin_body_entered(body):
+	if body is Player:
+		if(!collected): collect()
+
+#func _on_AudioStreamPlayer_finished():
+	#queue_free()
--- Scenes/Objects/Player.gd
+++ Scenes/Objects/Player.gd
@@ -1,14 +1,15 @@
 extends KinematicBody2D
-
+class_name Player
 # Stats
-var gSS = 64;
+var gSS = 64; #gridSpaceSize
 var accelX = gSS*1;
-var gravity = gSS*0.3
+var gravity = gSS*0.7
 var friction_curr = gSS*0.5
-var jumpStr = -gSS*8;
+var jumpStr = -gSS*15;
 var jump_count = 1
-var MAX_VEL_X = gSS*5;
+var MAX_VEL_X = gSS*7;
 var MAX_VEL_Y = gSS*20;
+var snap = Vector2(0,8);
 
 # States
 var facing = 1
@@ -17,6 +18,13 @@ var jumps_left = jump_count
 var velocity = Vector2.ZERO
 var Move = Vector2.ZERO
 
+enum STATE {
+	IDLE,
+	RUN,
+	AIR_NEUTRAL
+}
+
+var playerState = STATE.IDLE
 
 
 
@@ -27,14 +35,16 @@ func _ready():
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _physics_process(delta):
-	var MoveX = Input.get_action_strength("ui_right") - Input.get_action_strength("ui_left")
-	var MoveY = Input.get_action_strength("ui_down") - Input.get_action_strength("ui_up")
-	Move = Vector2(MoveX,MoveY).normalized()
+	do_playerInput();
+	# reset snap
+	snap = Vector2(0,gSS*0.3);
 	
 	# ------ do movement -------
 	if(Move.x != 0):
+		if is_on_floor(): playerState = STATE.RUN
 		velocity.x += Move.x*accelX;
 		facing = Move.x;
+		
 		
 	#friction
 	if(Move.x == 0): 
@@ -44,28 +54,64 @@ func _physics_process(delta):
 			else:
 				velocity.x -= friction_curr*sign(velocity.x)
 			
+			
 	
 	if is_on_floor():
+		if(Move.x == 0): playerState = STATE.IDLE
 		jumps_left = jump_count;
-	else:
+	else: #AIR BOURNE
+		playerState = STATE.AIR_NEUTRAL
 		velocity.y += gravity;
+		
 		
 	
 	#Jump code
-	var isJumping = Input.is_action_just_pressed("ui_up")
+	var isJumping = Input.is_key_pressed(KEY_W) || Input.is_key_pressed(KEY_SPACE)
+	#var isJumping = Input
 	
 	if(isJumping):
 		if(jumps_left > 0):
 			print("jump")
 			velocity.y = jumpStr;
 			jumps_left -= 1
+			snap *= 0;
 	
 	velocity.x = clamp(velocity.x , -MAX_VEL_X, MAX_VEL_X)
 	velocity.y = clamp(velocity.y, -MAX_VEL_Y, MAX_VEL_Y)
 	
-	velocity = move_and_slide(velocity, Vector2.UP)
+	velocity = move_and_slide_with_snap(velocity, snap, Vector2.UP)
+	#velocity = move_and_slide(velocity, Vector2.UP)
 	
+	do_playerAnimation()
+
+func do_playerInput():
+	var MoveX = Input.get_action_strength("ui_right") - Input.get_action_strength("ui_left")
+	var MoveX1 = int(Input.is_key_pressed(KEY_D)) - int(Input.is_key_pressed(KEY_A))
+	var MoveY = Input.get_action_strength("ui_down") - Input.get_action_strength("ui_up")
+	var MoveY1 = int(Input.is_key_pressed(KEY_S)) - int(Input.is_key_pressed(KEY_W))
+	Move = Vector2(MoveX1,MoveY1).normalized()
+	
+func do_playerAnimation():
 	if(facing == -1):
 		$AnimatedSprite.flip_h = true
 	elif(facing == 1):
 		$AnimatedSprite.flip_h = false
+		
+	match(playerState):
+		STATE.IDLE:
+			$AnimatedSprite.animation = "Idle"
+		STATE.RUN:
+			$AnimatedSprite.animation = "Run"
+			$AnimatedSprite.playing = true
+		STATE.AIR_NEUTRAL:
+			$AnimatedSprite.animation = "Air Neutral"
+			if(velocity.y < 0): #going up
+				if(abs(velocity.x) > gSS*2): $AnimatedSprite.frame = 3
+				else:  $AnimatedSprite.frame = 2
+			else: #going down
+				if(abs(velocity.x) > gSS*2): $AnimatedSprite.frame = 1
+				else:  $AnimatedSprite.frame = 0
+				
+
+func is_player():
+	return true
