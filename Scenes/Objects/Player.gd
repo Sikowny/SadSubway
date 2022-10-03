7d8af1860806c0b99ad5ca6adbc50d7f930d01be
Sikowny <bsickowny@hotmail,com>
2022-10-02 22:03:28 -0700

    yeha

--- Scenes/Objects/Player.gd
+++ Scenes/Objects/Player.gd
@@ -161,10 +161,10 @@ func _on_jumpBuffer_timeout():
 
 
 func _on_deathTimer_timeout():
-	restart_level()
+	Global.game_over()
 	
 func restart_level():
-	Global.restart_level()
+	pass
 	#var restart_ = owner.filename
 	#owner.queue_free();
 	#owner.get_parent().add_child(load(restart_).instance())
